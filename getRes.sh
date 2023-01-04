0b90503bbbb188fa05eeed23fa48b9b3f413ed88
mathewdblewis <40349774+mathewdblewis@users.noreply.github.com>
2023-01-03 21:41:07 -0800

    Update getRes.sh

--- getRes.sh
+++ getRes.sh
@@ -3,6 +3,4 @@ docker exec `docker run -dit app` /bin/bash -c \
 'source activate dedalus2; cd files; python3 search.py; tar -czf temp.zip output; python3 disp.py temp.zip' > res.txt
 python3 decode.py
 rm res.txt
-# tar -xf res.zip
-
-
+tar -xf res.zip
